bdc628455331e8fe6b21f464e9ee96db0de1b605
andrewcbuensalida <andrewcbuensalida@gmail.com>
2021-11-23 21:13:33 -0800

    testing auto deploy

--- src/Widgets/Widgets.js
+++ src/Widgets/Widgets.js
@@ -1,35 +1,39 @@
-import React from 'react'
-import './Widgets.css';
-import InfoIcon from '@material-ui/icons/Info';
-import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import React from "react";
+import "./Widgets.css";
+import InfoIcon from "@material-ui/icons/Info";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 function Widgets() {
+	const newsArticle = (heading, subtitle) => (
+		<div className="widgets__article">
+			<div className="widgets__articleLeft">
+				<FiberManualRecordIcon />
+			</div>
+			<div className="widgets__articleRight">
+				<h4>{heading}</h4>
+				<p>{subtitle}</p>
+			</div>
+		</div>
+	);
 
-    const newsArticle = (heading, subtitle) => (
-        <div className="widgets__article">
-            <div className="widgets__articleLeft">
-                <FiberManualRecordIcon/>
-            </div>
-            <div className="widgets__articleRight">
-                <h4>{heading}</h4>
-                <p>{ subtitle}</p>
-            </div>
-        </div>
-    )
+	return (
+		<div className="widgets">
+			<div className="widgets__header">
+				<h2>News</h2>
+				<InfoIcon />
+			</div>
 
-    return (
-        <div className="widgets">
-            <div className="widgets__header">
-                <h2>Atakans News</h2>
-                <InfoIcon />
-            </div>
-
-            {newsArticle("Atakan Geri geldi ", "En iyi haberler burdaa")}
-            {newsArticle("Corona bad", "Tadımız Kaçtı")}
-            {newsArticle("Ejderhalar geliyor ", "Sıcak iklim yanii")}
-
-        </div>
-    );
+			{newsArticle(
+				"Andrew gets a job!",
+				"Mr. Buensalida finally becomes a software engineer."
+			)}
+			{newsArticle(
+				"Covid is gone for good",
+				"The vaccines and social distancing worked."
+			)}
+			{newsArticle("Cancer is cured", "AI discovered it")}
+		</div>
+	);
 }
 
 export default Widgets;
